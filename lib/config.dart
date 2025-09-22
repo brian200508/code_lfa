9c586c82ca0aee707232bb4eff4e918aea2bfc12
brian200508 <36002167+brian200508@users.noreply.github.com>
2025-09-22 07:26:28 +0000

    update code-server to v4.104.1

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 const bool product = bool.fromEnvironment('dart.vm.product');
-const String debugCSV = '4.104.0';
+const String debugCSV = '4.104.1';
 
 class Config {
   Config._();
--- scripts/check_hardlink.sh
+++ scripts/check_hardlink.sh
@@ -1,1 +1,1 @@
-tar tvf 'assets/code-server-4.104.0-linux-arm64.tar.gz' | grep '^hr'
+tar tvf 'assets/code-server-4.104.1-linux-arm64.tar.gz' | grep '^hr'
