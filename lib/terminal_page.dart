bb490682bda22d89c41cf25161b91a0286532ffe
brian200508 <36002167+brian200508@users.noreply.github.com>
2026-08-24 17:19:18 +0200

    Integrate custom keyboard bar into terminal page

--- lib/terminal_page.dart
+++ lib/terminal_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
 import 'package:xterm/xterm.dart';
+import 'custom_keyboard_bar.dart';
 import 'terminal_controller.dart';
 import 'terminal_theme.dart';
 
@@ -18,6 +19,19 @@ class _TerminalPageState extends State<TerminalPage> {
   HomeController controller = Get.put(HomeController());
   ManjaroTerminalTheme terminalTheme = ManjaroTerminalTheme();
   bool visible = false || kDebugMode;
+  late CustomKeyboardController keyboardController;
+
+  @override
+  void initState() {
+    super.initState();
+    keyboardController = CustomKeyboardController(terminal: controller.terminal);
+  }
+
+  @override
+  void dispose() {
+    keyboardController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,87 +50,97 @@ class _TerminalPageState extends State<TerminalPage> {
               setState(() {});
             },
             behavior: HitTestBehavior.translucent,
-            child: Stack(
-              alignment: Alignment.center,
+            child: Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.all(8.w),
-                  child: Visibility(
-                    visible: visible,
-                    // IgnorePointer
-                    child: AbsorbPointer(
-                      absorbing: false,
-                      child: TerminalView(
-                        controller.terminal,
-                        readOnly: false,
-                        backgroundOpacity: 1,
-                        theme: ManjaroTerminalTheme(),
-                      ),
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Material(
-                    borderRadius: BorderRadius.circular(12.w),
-                    color: Theme.of(context).colorScheme.surface,
-                    child: SizedBox(
-                      width: 300.w,
-                      child: Padding(
-                        padding: EdgeInsets.all(12.w),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Center(
-                              child: RepaintBoundary(
-                                child: LoadingProgress(
-                                  minRadius: 6,
-                                  strokeWidth: 3,
-                                  increaseRadius: 3,
-                                ),
-                              ),
+                Expanded(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.w),
+                        child: Visibility(
+                          visible: visible,
+                          child: AbsorbPointer(
+                            absorbing: false,
+                            child: TerminalView(
+                              controller.terminal,
+                              readOnly: false,
+                              backgroundOpacity: 1,
+                              theme: ManjaroTerminalTheme(),
                             ),
-                            SizedBox(height: 12.w),
-                            GetBuilder<HomeController>(builder: (controller) {
-                              return Column(
-                                children: [
-                                  Stack(
-                                    children: [
-                                      Container(
-                                        height: 5.w,
-                                        decoration: BoxDecoration(
-                                          color: Theme.of(context).colorScheme.primary.withAlpha((0.2 * 255).toInt()),
-                                          borderRadius: BorderRadius.circular(3.w),
-                                        ),
-                                      ),
-                                      AnimatedContainer(
-                                        duration: 300.milliseconds,
-                                        height: 5.w,
-                                        width: 300.w * controller.progress,
-                                        decoration: BoxDecoration(
-                                          color: Theme.of(context).colorScheme.primary,
-                                          borderRadius: BorderRadius.circular(3.w),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(height: 8.w),
-                                  Text(
-                                    controller.currentProgress.trim(),
-                                    style: TextStyle(
-                                      fontSize: 12.w,
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context).colorScheme.onSurface,
-                                    ),
-                                  ),
-                                ],
-                              );
-                            }),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
+                      Center(
+                        child: Material(
+                          borderRadius: BorderRadius.circular(12.w),
+                          color: Theme.of(context).colorScheme.surface,
+                          child: SizedBox(
+                            width: 300.w,
+                            child: Padding(
+                              padding: EdgeInsets.all(12.w),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Center(
+                                    child: RepaintBoundary(
+                                      child: LoadingProgress(
+                                        minRadius: 6,
+                                        strokeWidth: 3,
+                                        increaseRadius: 3,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 12.w),
+                                  GetBuilder<HomeController>(builder: (controller) {
+                                    return Column(
+                                      children: [
+                                        Stack(
+                                          children: [
+                                            Container(
+                                              height: 5.w,
+                                              decoration: BoxDecoration(
+                                                color: Theme.of(context).colorScheme.primary.withAlpha((0.2 * 255).toInt()),
+                                                borderRadius: BorderRadius.circular(3.w),
+                                              ),
+                                            ),
+                                            AnimatedContainer(
+                                              duration: 300.milliseconds,
+                                              height: 5.w,
+                                              width: 300.w * controller.progress,
+                                              decoration: BoxDecoration(
+                                                color: Theme.of(context).colorScheme.primary,
+                                                borderRadius: BorderRadius.circular(3.w),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(height: 8.w),
+                                        Text(
+                                          controller.currentProgress.trim(),
+                                          style: TextStyle(
+                                            fontSize: 12.w,
+                                            fontWeight: FontWeight.bold,
+                                            color: Theme.of(context).colorScheme.onSurface,
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  }),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                // Custom Keyboard Bar
+                CustomKeyboardBar(
+                  controller: keyboardController,
+                  alignment: MainAxisAlignment.start,
                 ),
               ],
             ),
